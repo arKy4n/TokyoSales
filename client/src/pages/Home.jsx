dbb5d380870c87b637e660d54055fafa7214d6f0
arKy4n <rkmvaryan@gmail.com>
2024-02-27 19:29:19 +0900

    Product display complete

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,49 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:5000/product/display"
+        );
+
+        // console.log(response.data.productData);
+
+        setProducts(response.data.productData);
+      } catch (err) {
+        console.log("Error Message: ", err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <h1>Home Page</h1>
+      <h2>Products</h2>
+      {/* <pre>{JSON.stringify(products, null, 2)}</pre> */}
+      <div className="products-container">
+        {products.map((product) => (
+          <div key={product.productid}>
+            <h3>{product.productname}</h3>
+            <p>{product.description}</p>
+            <p>{product.image_path}</p>
+            <img
+              src={`http://localhost:5000/uploads/${product.image_path}`}
+              alt={product.productname}
+            />
+          </div>
+        ))}
+      </div>
+      {/* <img
+        src={"http://localhost:5000/Storage/1708602205338-Parrot.png"}
+        alt="name"
+      /> */}
     </>
   );
 }
